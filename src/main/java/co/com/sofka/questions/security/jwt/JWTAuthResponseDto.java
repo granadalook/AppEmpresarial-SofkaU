19774b7b2788b5ceea286f5848e56794a3abaac2
AngelaEspinosa2021 <titagena@gmail.com>
2022-06-11 05:49:11 -0500

    FIX: Se logra solución parcial para el fallo que presenta el Back en el proce de login y creacion de usuarios.

---
+++ src/main/java/co/com/sofka/questions/security/jwt/JWTAuthResponseDto.java
@@ -0,0 +1,2 @@
+package co.com.sofka.questions.security.jwt;public class JWTAuthResponseDto {
+}
--- src/main/java/co/com/sofka/questions/security/services/RolService.java
+++ src/main/java/co/com/sofka/questions/security/services/RolService.java
@@ -1,23 +1,2 @@
-package co.com.sofka.questions.security.services;
-
-import co.com.sofka.questions.security.collections.Rol;
-import co.com.sofka.questions.security.repositories.RolRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.Optional;
-
-@Service
-public class RolService {
-
-    @Autowired(required = true)
-    RolRepository rolRepository;
-
-    public Optional<Rol> getByRolName(String rolName) {
-        return rolRepository.findByRolName(rolName);
-    }
-
-    public void save(Rol rol) {
-        rolRepository.save(rol);
-    }
+package co.com.sofka.questions.security.services;public class RolService {
 }
