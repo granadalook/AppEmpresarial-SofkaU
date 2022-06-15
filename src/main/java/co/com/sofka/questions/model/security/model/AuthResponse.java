65c971b046fb24ea828284b5d89730f5e9d8e7a6
granadalook <tiven17.jsga@gmail.com>
2022-06-14 22:15:09 -0500

    CREATE: Se crea la clase AuthResponse para JWT

---
+++ src/main/java/co/com/sofka/questions/model/security/model/AuthResponse.java
@@ -0,0 +1,16 @@
+package co.com.sofka.questions.model.security.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author ard333
+ */
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class AuthResponse {
+    private String token;
+
+}
