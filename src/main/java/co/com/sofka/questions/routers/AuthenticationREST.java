04094a46724bc7f7240d5d6a6a297e2b51592b83
granadalook <tiven17.jsga@gmail.com>
2022-06-14 22:20:11 -0500

    CREATE: se crea la clase  AuthenticationREST para autenticar las peticines del fron por JWT

---
+++ src/main/java/co/com/sofka/questions/routers/AuthenticationREST.java
@@ -0,0 +1,34 @@
+package co.com.sofka.questions.routers;
+
+
+import co.com.sofka.questions.model.security.AuthRequest;
+import co.com.sofka.questions.model.security.JWTUtil;
+import co.com.sofka.questions.model.security.PBKDF2Encoder;
+import co.com.sofka.questions.model.security.model.AuthResponse;
+import co.com.sofka.questions.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@AllArgsConstructor
+@RestController
+public class AuthenticationREST {
+
+    private JWTUtil jwtUtil;
+    private PBKDF2Encoder passwordEncoder;
+    private UserService userService;
+
+    @PostMapping("/login")
+    public Mono<ResponseEntity<AuthResponse>> login(@RequestBody AuthRequest ar) {
+        return userService.findByUsername(ar.getUsername())
+                .filter(userDetails -> passwordEncoder.encode(ar.getPassword()).equals(userDetails.getPassword()))
+                .map(userDetails -> ResponseEntity.ok(new AuthResponse(jwtUtil.generateToken(userDetails))))
+                .switchIfEmpty(Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()));
+    }
+
+}
